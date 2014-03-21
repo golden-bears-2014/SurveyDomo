1def9211904f06454f64515940a65739321d9472
Christine Dolendo <chris.dolendo@gmail.com>
2014-03-21 16:41:59 -0700

    commented out shoulda matchers from env

--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,7 @@ require "sinatra/reloader" if development?
 require 'erb'
 require 'csv'
 require 'faker'
-require 'shoulda-matchers'
+# require 'shoulda-matchers'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
