357c95de48ae44b6a20173cc2f4b249e639acccc
Germain Boue & Michael Gleason <pair+boue.fireflypulse@devbootcamp.com>
2014-03-22 18:10:20 -0700

    Deleted old commented code in survey model.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -21,32 +21,4 @@ class Survey < ActiveRecord::Base
   end
 
 
-  # def self.create(params)
-
-  #   # p params["survey_name"]
-
-  #   s = self.new
-  #   s.name = (params["survey_name"])
-  #   s.save
-  #   3.times do |index|
-  #     s.questions << Question.create(content: params["question#{index+1}"])
-  #     4.times do |cindex|
-  #       s.questions.all[index].choices << Choice.create(content: params["choices#{index+1}"]["choice#{cindex+1}"])
-  #     end
-  #   end
-
-  #   params.each_pair do |key,value|
-
-  #     if key == "survey_name"
-  #       s.name = value
-  #     elsif key.match("question")
-  #       s.questions << Question.create
-
-  #   end
-
-  #   s.save!
-  #   return s
-
-  # end
-
 end
